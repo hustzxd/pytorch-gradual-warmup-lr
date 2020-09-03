6847322c89fcb57f465f4d3d517645383d4cf7d1
2227820226@qq.com <2227820226@qq.com>
2020-09-03 15:55:20 +0800

    :bug: for 1.4.0 pytorch;

--- warmup_scheduler/run.py
+++ warmup_scheduler/run.py
@@ -1,5 +1,5 @@
 import torch
-from torch.optim.lr_scheduler import StepLR, ExponentialLR
+from torch.optim.lr_scheduler import StepLR, ExponentialLR, CosineAnnealingLR
 from torch.optim.sgd import SGD
 
 from warmup_scheduler import GradualWarmupScheduler
@@ -11,14 +11,16 @@ if __name__ == '__main__':
 
     # scheduler_warmup is chained with schduler_steplr
     scheduler_steplr = StepLR(optim, step_size=10, gamma=0.1)
-    scheduler_warmup = GradualWarmupScheduler(optim, multiplier=1, total_epoch=5, after_scheduler=scheduler_steplr)
+
+    scheduler_cosinelr = CosineAnnealingLR(optim, T_max=40)
+    scheduler_warmup = GradualWarmupScheduler(optim, multiplier=2, total_epoch=10, after_scheduler=scheduler_cosinelr)
 
     # this zero gradient update is needed to avoid a warning message, issue #8.
     optim.zero_grad()
     optim.step()
 
-    for epoch in range(1, 20):
-        scheduler_warmup.step(epoch)
+    for epoch in range(0, 40):
+        scheduler_warmup.step()
         print(epoch, optim.param_groups[0]['lr'])
 
         optim.step()    # backward pass (update network)
